a69549b075670f45f234ca6a70fa8c494df48138
Sivaramalingam Kirushanth <sivaramalingamk@gmail.com>
2017-10-08 05:55:53 +0200

    Just added a linear layout with some tools

--- app/src/main/java/za/ac/cput/fidit/infoshareapp/MainActivity.java
+++ app/src/main/java/za/ac/cput/fidit/infoshareapp/MainActivity.java
@@ -1,6 +1,7 @@
 package za.ac.cput.fidit.infoshareapp;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -21,6 +25,8 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        LinearLayout coordinatorLayout=(LinearLayout) findViewById(R.id.coord_lay);
+             // coordinatorLayout.setOnClickListener();
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -31,6 +37,8 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
