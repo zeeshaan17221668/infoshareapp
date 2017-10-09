9e55aba06221979bef8bace66b2199cc45e84d0e
zeeshaan <zeeshaan17221668@gmail.com>
2017-10-09 15:39:12 +0200

    just a test

--- app/src/main/java/za/ac/cput/fidit/infoshareapp/MainActivity.java
+++ app/src/main/java/za/ac/cput/fidit/infoshareapp/MainActivity.java
@@ -101,7 +101,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
-// just a simply crazy line
+// zeeshaan was here...
        
             DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
             drawer.closeDrawer(GravityCompat.START);
